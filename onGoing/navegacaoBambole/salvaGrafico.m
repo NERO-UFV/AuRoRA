0e7eaea19dce8b328b58f7235a4968acb9cd63ab
Alexandre-Caldeira <alexandregomescaldeira@gmail.com>
2020-10-25 19:42:01 -0300

    Changed salvaGrafico.m and README

--- onGoing/navegacaoBambole/salvaGrafico.m
+++ onGoing/navegacaoBambole/salvaGrafico.m
@@ -3,11 +3,21 @@ function salvaGrafico(nome)
 %   Tomando nome como string de entrada, salva .pdf
 %   vetorizando a figura aberta por último
     
+    try 
+    	cd imagens
+    catch
+        mkdir imagens
+        cd imagens
+    end 
+
     set(gcf,'Units','inches');
     screenposition = get(gcf,'Position');
     set(gcf,...
         'PaperPosition',[0 0 screenposition(3:4)],...
         'PaperSize',[screenposition(3:4)]);
     print(gcf,'-dpdf',nome)
+    print(gcf,'-dsvg',nome)
+    
+    cd ..
 end
 
